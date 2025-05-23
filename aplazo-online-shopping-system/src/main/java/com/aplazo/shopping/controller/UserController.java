e5ae7ae546c77d01a6124028675d7a4e6ab9cd92
CesarSalazarCayetano <cesarsalazarcayetano@gmail.com>
2025-05-23 16:10:55 -0600

    Se agregan las dependencias para el uso de RateLimit, se agrega un controlador para probar el uso de RateLimit, se agregan las configuraciones al properties

---
+++ aplazo-online-shopping-system/src/main/java/com/aplazo/shopping/controller/UserController.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.aplazo.shopping.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
+/**
+ * @author CesarSalazar
+ */
+@RestController
+@RequestMapping("/public/user")
+public class UserController {
+	
+	@RateLimiter(name = "public-api")
+	@GetMapping("/test")
+	public ResponseEntity<?> getMethodName() {
+		return ResponseEntity.ok("hello world");
+	}
+	
+}
