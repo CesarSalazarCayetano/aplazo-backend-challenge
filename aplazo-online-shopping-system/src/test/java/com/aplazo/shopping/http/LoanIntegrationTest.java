3575b263abdc990748bab1445f64e36320366b52
CesarSalazarCayetano <cesarsalazarcayetano@gmail.com>
2025-05-26 11:49:46 -0600

    Se agrega la clase de prueba para loan

---
+++ aplazo-online-shopping-system/src/test/java/com/aplazo/shopping/http/LoanIntegrationTest.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.aplazo.shopping.http;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author CesarSalazar
+ */
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class LoanIntegrationTest {
+
+	@LocalServerPort
+	private int port;
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	private static final String CONTEXT_PATH = "/api/v1";
+	private static final String LOAN_PATH = "/protected/loans";
+	private static final String BASE_API = "http://localhost:";
+	
+	@DisplayName("The call at endpoint create, must be fails because i dont send the HEADER and token.")
+	@Test
+	void shouldReturn401WhenNotSendHeaderAuthorization() throws JsonProcessingException {
+		var urlApi = BASE_API + this.port + CONTEXT_PATH + LOAN_PATH;
+		
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(MediaType.APPLICATION_JSON);
+//		header.add(CONTEXT_PATH, BASE_API);
+		
+		Map<String, Object> bodyParamMap = new HashMap<String, Object>();
+
+		bodyParamMap.put("idCustomer", "9e6256ec-1cba-4c4f-b9ea-475599bf6a2b");
+		bodyParamMap.put("amount", 1000.00D);
+		
+		String reqBodyData = new ObjectMapper().writeValueAsString(bodyParamMap);
+
+		HttpEntity<String> requestEnty = new HttpEntity<>(reqBodyData, header);
+		
+		ResponseEntity<Object> response = this.restTemplate.postForEntity(urlApi, requestEnty, Object.class);
+		
+		assertEquals(response.getStatusCode().value(), 401, "The http code must be 401");
+	}
+}
